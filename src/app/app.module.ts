4b32db88c4eceb4897f204da1a4a01e3cfb72cee
jelenamarjancevic <jelenamarjancevic11@gmail.com>
2025-11-12 19:56:40 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArtiklComponent } from './main/artikl/artikl.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtiklComponent
   ],
   imports: [
     BrowserModule
